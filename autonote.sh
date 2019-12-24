64d72f501be67f26020d8fd1aa5a2719ad67d3c0
Erdoğan Yoksul <erdoganyoksul3@gmail.com>
2019-12-24 09:28:26 +0300

    Update autonote.sh

--- autonote.sh
+++ autonote.sh
@@ -2,17 +2,17 @@
 
 # AUTHOR: Eredot_PK&FR       0=={::::::::::::>
 # -----------------------------------------------------------------------------------
-# Follow Me On Twitter (https://twitter.com/eredot_pkfr)
+# Follow Me On Twitter (https://twitter.com/YoksulErdogan?lang=tr)
 # Follow Me On GitHub (https://github.com/eredotpkfr)
-# Follow Me On Linkedin (https://www.linkedin.com/in/eredotpkfr/)
+# Follow Me On Linkedin (https://www.linkedin.com/in/erdo%C4%9Fan-yoksul-47897a151/)
 # Follow Me On My Blog (https://www.erdoganyoksul.com/)
 # -----------------------------------------------------------------------------------
-
 
 function delete_files(){
     deleted_files=`git status | grep "deleted:" | awk '{print $2}'`
     if [ ! -z "$deleted_files" ]; then
-        zenity --question --width 333 --text="Are you sure ? Do you want to delete this/these files ?\n $(echo -n $deleted_files | tr ' ' '\t')"
+        zenity --question --width 333 --text="Are you sure ? Do you want to \
+		delete this/these files ?\n $(echo -n $deleted_files | tr ' ' '\t')"
         if [ $? -eq 0 ]; then
             git rm $deleted_files
         else :
@@ -23,7 +23,8 @@ function delete_files(){
 function push_files(){
     cd /home/eredot_pkfr/n0t3s/notes/ && git add *
     if [[ "`git status`" == *"nothing to commit"* ]]; then
-        if [[ "`git status`" == *"nothing to commit"* && "`git status`" == *"(use \"git push\" to publish your local commits)"* ]]; then
+        if [[ "`git status`" == *"nothing to commit"* && "`git status`" == \
+		*"(use \"git push\" to publish your local commits)"* ]]; then
 	    git push -u origin master --force
             notify-send "AUTONOTE - Your Repository Is UP-TO-DATE"
 	else
